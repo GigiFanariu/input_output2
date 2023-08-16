ef3e1509ff7fb4e8bdc51bd6d3a03d7158ead030
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-08-16 23:29:19 +0200

    Corretto

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        String firstName = "Gigi";
+        String lastName = "Fanariu";
+
+        System.out.println(firstName +" "+ lastName);
+    }
+}
